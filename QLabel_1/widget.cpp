0364f81133026f58833f9e867e18e7fb7e032b5c
CODE_OF_ZHANG <3363967646@qq.com>
2025-07-15 23:52:27 +0800

    QLabel常见的文本格式

---
+++ QLabel_1/widget.cpp
@@ -0,0 +1,26 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    // 把第一个 label 设置成显示纯文本
+    ui->label->setTextFormat(Qt::PlainText);
+    ui->label->setText("这是纯文本");
+
+    // 把第二个 label 设置成富文本
+    ui->label_2->setTextFormat(Qt::RichText);
+    ui->label_2->setText("<b>这是富文本</b>");
+
+    // 把第三个 label 设置成 markdown
+    ui->label_3->setTextFormat(Qt::MarkdownText);
+    ui->label_3->setText("# 这是 markdown 文本");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
