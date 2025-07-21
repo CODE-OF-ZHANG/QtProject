c9923e00874c840750be40e641b594bda3ff2ff9
CODE_OF_ZHANG <3363967646@qq.com>
2025-07-21 23:30:28 +0800

    QLabel设置自动换行，边距，缩进

--- QLabel_3/widget.cpp
+++ QLabel_3/widget.cpp
@@ -7,15 +7,32 @@ Widget::Widget(QWidget *parent)
 {
     ui->setupUi(this);
     ui->label->setText("这是一段文本");
-    ui->label_2->setText("这是一段文本");
-    ui->label_3->setText("这是一段文本");
-    ui->label_4->setText("这是一段文本");
+//    ui->label_2->setText("这是一段文本");
+//    ui->label_3->setText("这是一段文本");
+//    ui->label_4->setText("这是一段文本");
 
     // 设置对齐方式
     ui->label->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
-    ui->label_2->setAlignment(Qt::AlignLeft | Qt::AlignCenter);
-    ui->label_3->setAlignment(Qt::AlignTop | Qt::AlignLeft);
-    ui->label_4->setAlignment(Qt::AlignRight | Qt::AlignRight);
+//    ui->label_2->setAlignment(Qt::AlignLeft | Qt::AlignCenter);
+//    ui->label_3->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+//    ui->label_4->setAlignment(Qt::AlignRight | Qt::AlignRight);
+
+    // 设置自动换行
+    ui->label_2->setText("这是一段很长的文本这是一段很长的文本这是一段很长的文本这是一段很长的文本这是一段很长的文本");
+    ui->label_2->setWordWrap(true);
+
+
+    // 设置缩进
+    ui->label_3->setText("这是一段很长的文本这是一段很长的文本这"
+                         "是一段很长的文本这是一段很长的文本这是一段很长的文本");
+    ui->label_3->setWordWrap(true);
+    ui->label_3->setIndent(50);
+
+    // 设置边距
+    ui->label_4->setText("这是一段很长的文本这是一段很长的文本这"
+                         "是一段很长的文本这是一段很长的文本这是一段很长的文本");
+    ui->label_4->setWordWrap(true);
+    ui->label_4->setMargin(50);
 }
 
 Widget::~Widget()
