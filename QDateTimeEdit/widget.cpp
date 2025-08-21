cfeda78f343e1c7ac427c0a68d62a6801004795d
CODE_OF_ZHANG <3363967646@qq.com>
2025-08-21 23:51:32 +0800

    QDateTimeEdit的使用

---
+++ QDateTimeEdit/widget.cpp
@@ -0,0 +1,35 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    // 获取时间
+    QDateTime timeOld = ui->dateTimeEdit->dateTime();
+    QDateTime timeNew = ui->dateTimeEdit_2->dateTime();
+
+    // 计算时间差值
+    int days = timeOld.daysTo(timeNew);
+    int seconds = timeOld.secsTo(timeNew);
+
+    // 秒换算成小时
+    int hours = (seconds / 3600) % 24;
+
+    // 将结果放入 label 中
+    ui->label->setText(QString("爱你已经持续了") + QString::number(days)
+                       + QString("天零") + QString::number(hours)
+                       + QString(" 小时！"));
+}
